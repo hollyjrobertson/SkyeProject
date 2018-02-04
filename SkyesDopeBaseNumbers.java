95ca6f95b7035644eecec287e9403700f7b9e2aa
hollyjoc <holly.jo.robertson22@gmail.com>
2018-02-03 19:36:54 -0500

    Skye's changes

--- SkyesDopeBaseNumbers.java
+++ SkyesDopeBaseNumbers.java
@@ -6,58 +6,65 @@ import java.util.*;
 public class SkyesDopeBaseNumbers
 {
     static List<String> list = new ArrayList<String>();
-    static int base;
+    static int base = 4;
     static int length = 3;
-    
+
     public static void main(String[] args)
     {
         listSetup();
         //listDriller();
+        generateAllStringsHelper(length, base);
+        
+        System.out.println(list);
     }
-    
+
     public static void listSetup()
     {
         while(list.size()<length)
         {
             list.add(String.valueOf(0));
         }
-        System.out.println(list);
+//        System.out.println(list);
     }
-}
-    
-//    public static void listDriller()
-//    {
-//       
+
+    public static void listDriller()
+    {
+//
 //        int drill = Integer.valueOf(list.get(length-1));
-//        
-//        
+//
+//
 //        /*
 //       for(int i = 0; i<length; i++)
 //       {
-//           
+//
 //       }
-//    }*/
-//    }
-   
-    
-//    public static List<String> generateAllStringsHelper(int length, int base)
-//    {   
-//        if (length < 1)
-//        {
-//            return list;
-//        }
-//        
-//        
-//        length--;
-//    }
-        
-    /*
+    }
+
+
+   public static th < 1)
+        {
+            return list;
+       }List<String> generateAllStringsHelper(int length, int base)
+    {
+        if (leng
+
+          for(int drill = Integer.valueOf(list.get(length-1)); drill<base; drill++)
+          {
+            list.set(length - 1, String.valueOf(drill));
+            System.out.println(list);
+          }
+          
+          
+          return generateAllStringsHelper(length-1, base);
+    }
+
+//TEMPORARY
     public static void incrementer()
     {
         int baseBitch = 0;
         if(baseBitch < base)
         {
-            
+
         }
     }
-}*/
+}
